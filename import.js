b1081010076b212699beb795cbd3c62f16e04bf0
Jonas Luebbers <jonaskluebbers@gmail.com>
2021-03-02 13:13:56 -0500

    Add posts

--- import.js
+++ import.js
@@ -1,6 +1,9 @@
 import { SAXParser } from "https://deno.land/x/xmlp/mod.ts";
 import { Machine, interpret, assign } from "https://cdn.pika.dev/xstate";
 
+await Deno.mkdir("pages", { recursive: true });
+await Deno.mkdir("posts", { recursive: true });
+
 function writeItem(item) {
   if (item["wp:post_type"] === "page") {
     const { "content:encoded": initialContent, link, ...data } = item;
@@ -18,6 +21,25 @@ function writeItem(item) {
       `pages${link}.html`,
       `---json\n${frontmatter}\n---\n\n${content}`
     );
+  } else if (item["wp:post_type"] === "post") {
+    const { "content:encoded": initialContent, link, ...data } = item;
+    const frontmatter = JSON.stringify({
+      layout: "layouts/page.liquid",
+      ...data,
+    });
+    if (initialContent) {
+      const content = initialContent
+        .replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, "")
+        .replace(
+          /<noscript\b[^<]*((?:(?!<\/noscript>)<[^<]*)*)<\/noscript>/gi,
+          (_, p1) => p1
+        );
+      const shortLink = link.match(/[\w-]*$/gi)[0];
+      Deno.writeTextFile(
+        `posts/${shortLink}.html`,
+        `---json\n${frontmatter}\n---\n\n${content}`
+      );
+    }
   }
 }
 
