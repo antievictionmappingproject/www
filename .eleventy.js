b0ec3fab659f2108a6512000e3707e301120577c
Jonas Luebbers <jonaskluebbers@gmail.com>
2021-03-08 11:47:34 -0500

    Add postList partial

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,13 @@
-const locales = ["en", "es"];
-
 const CleanCSS = require("clean-css");
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addGlobalData("locales", locales);
-
   eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
 
   eleventyConfig.addPassthroughCopy("admin");
 
-  locales.forEach((locale) => {
+  ["en", "es"].forEach((locale) => {
     eleventyConfig.addCollection(`posts_${locale}`, function (collectionApi) {
       return collectionApi.getFilteredByGlob(`posts/${locale}/*.md`);
     });
