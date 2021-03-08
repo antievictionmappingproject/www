5d31a40c306e48d4fb890ca075da088dff9b7ad1
Jonas Luebbers <jonaskluebbers@gmail.com>
2021-03-08 11:54:17 -0500

    Add data file for locales

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+const locales = require("./_data/locales.json");
 const CleanCSS = require("clean-css");
 
 module.exports = function (eleventyConfig) {
@@ -7,7 +8,7 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("admin");
 
-  ["en", "es"].forEach((locale) => {
+  locales.forEach((locale) => {
     eleventyConfig.addCollection(`posts_${locale}`, function (collectionApi) {
       return collectionApi.getFilteredByGlob(`posts/${locale}/*.md`);
     });
