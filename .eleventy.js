578e171c32dcdeceae3c9a851e4f576a2633575c
Jonas Luebbers <jonaskluebbers@gmail.com>
2021-03-10 21:58:46 -0500

    Add eleventy-plugin-i18n

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,20 @@
 const CleanCSS = require("clean-css");
+const i18nPlugin = require("eleventy-plugin-i18n");
+
+const translations = require("./_data/translations.json");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.setDataDeepMerge(true);
 
   eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
+  });
+
+  eleventyConfig.addPlugin(i18nPlugin, {
+    translations,
+    fallbackLocales: {
+      "*": "en",
+    },
   });
 
   eleventyConfig.addPassthroughCopy("admin");
