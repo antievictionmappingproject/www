7b86cf01d56700b5a408c193dff5d34b0767b448
Jonas Luebbers <jonaskluebbers@gmail.com>
2021-04-02 19:35:03 -0400

    Remove clean-css

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,7 @@
-const CleanCSS = require("clean-css");
-
 const { translations } = require("./_data/translations.json");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.setDataDeepMerge(true);
-
-  eleventyConfig.addFilter("cssmin", function (code) {
-    return new CleanCSS({}).minify(code).styles;
-  });
 
   eleventyConfig.addFilter("translate", function (value) {
     const o = translations.find(({ en }) => en === value);
