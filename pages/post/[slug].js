33d37db205b3d52bde9683e137362dffab11800a
Jonas Luebbers <jonaskluebbers@gmail.com>
2021-12-04 17:34:31 -0500

    Add block content

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,15 +1,26 @@
 import groq from "groq";
 import { client } from "../../lib/sanity";
+import BlockContent from "@sanity/block-content-to-react";
+import Link from "next/link";
 
 export default function Post({ post }) {
-  return <div>{post.title}</div>;
+  return (
+    <article>
+      <Link href="/post">
+        <a>Back</a>
+      </Link>
+      <h1>{post.title}</h1>
+      <BlockContent blocks={post.body} />
+    </article>
+  );
 }
 
 export async function getStaticProps({ params, locale }) {
   const post = await client.fetch(
     groq`
     *[_type == "post" && slug.current == $slug][0] {
-      "title": title[$locale]
+      "title": title[$locale],
+      "body": body[$locale]
     }
   `,
     { slug: params.slug, locale }
