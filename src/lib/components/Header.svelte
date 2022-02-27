bf699d95302fc254d549aed8b49dd32bfd99843c
Jonas Luebbers <jonaskluebbers@gmail.com>
2022-02-27 16:10:40 -0500

    Add Combobox component

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,12 +1,20 @@
 <script>
   import { page } from "$app/stores";
+
+  let value = "";
 </script>
 
 <header>
   <form action="/{$page.params.locale}/search">
     <label>
       <span>Search:</span>
-      <input type="text" name="query" />
+      <input
+        type="text"
+        name="query"
+        bind:value
+        placeholder="Search"
+        autocomplete="off"
+      />
     </label>
     <button type="submit">Submit</button>
   </form>
