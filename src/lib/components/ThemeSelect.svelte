d860040881597917e8b97cecf82e9fdfb41015f5
Jonas Luebbers <jonas@jonasluebbers.com>
2022-09-16 15:39:12 -0700

    Add ThemeSelect

---
+++ src/lib/components/ThemeSelect.svelte
@@ -0,0 +1,27 @@
+<script>
+  import {onMount} from 'svelte'
+
+  let value = 'system'
+  let isMounted = false
+
+  $: {
+    if (isMounted) {
+      console.log('update')
+      localStorage.setItem('theme', value)
+      document.documentElement.dataset.theme = value
+    }
+  }
+
+  onMount(() => {
+    console.log('mount')
+    const theme = window.localStorage.getItem('theme')
+    value = theme === 'string' ? theme : 'system'
+    isMounted = true
+  })
+</script>
+
+<select bind:value>
+  <option value="light">Light</option>
+  <option value="dark">Dark</option>
+  <option value="system">System</option>
+</select>
