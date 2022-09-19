43a7ae01e8a0f61e47e9b3946ff2b1327de5b81e
Jonas Luebbers <jonas@jonasluebbers.com>
2022-09-19 13:05:50 -0700

    Add bootstrap SVG

--- src/lib/components/SearchForm.svelte
+++ src/lib/components/SearchForm.svelte
@@ -1,5 +1,7 @@
 <script>
+  import icons from 'bootstrap-icons/bootstrap-icons.svg'
   import {nextUniqueId} from '$lib/utils/uniqueId'
+  console.log(icons)
 
   let inputId = nextUniqueId()
   let listboxId = nextUniqueId()
@@ -24,7 +26,16 @@
       aria-label="Submit Search"
       aria-expanded={isExpanded ? 'true' : 'false'}
       aria-controls={listboxId}
-    />
+    >
+      <svg
+        fill="currentColor"
+        aria-hidden="true"
+        focusable="false"
+        style="forced-color-adjust: auto"
+      >
+        <use xlink:href={`${icons}#search`} />
+      </svg>
+    </button>
   </div>
   <ul id={listboxId} role="listbox" aria-label="States">
     {#each options as option}
@@ -40,3 +51,10 @@
     {/each}
   </ul>
 </div>
+
+<style>
+  svg {
+    width: 1rem;
+    height: 1rem;
+  }
+</style>
