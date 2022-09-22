2f1388845bed64284398fc33dab94e868203a3c0
Jonas Luebbers <jonas@jonasluebbers.com>
2022-09-22 15:54:56 -0700

    Update config

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,12 @@
 import adapter from '@sveltejs/adapter-netlify'
 import preprocess from 'svelte-preprocess'
-import path from 'path'
 
 const config = {
   preprocess: preprocess(),
   kit: {
     adapter: adapter({split: true}),
-    vite: {
-      resolve: {
-        alias: {
-          $i18n: path.resolve('./src/i18n')
-        }
-      }
+    alias: {
+      $i18n: 'src/i18n'
     }
   }
 }
