32747fce094b80659f32d620347c94915effdc8c
Jonas Luebbers <jonas@jonasluebbers.com>
2022-09-23 09:59:06 -0700

    Add alternate links to head

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,26 @@
 <script lang="ts">
+  import {page} from '$app/stores'
   import '@nonphoto/css/reset.css'
   import '$lib/global.css'
   import {setLocale} from '$i18n/i18n-svelte'
+  import {locales} from '$i18n/i18n-util'
   import type {LayoutData} from './$types'
+  import {replaceLocale} from '$lib/url'
+  import {each} from 'svelte/internal'
 
   export let data: LayoutData
 
   setLocale(data.locale)
 </script>
+
+<svelte:head>
+  {#each locales as locale}
+    <link
+      rel="alternate"
+      href={replaceLocale($page.url.pathname, locale)}
+      hreflang={locale}
+    />
+  {/each}
+</svelte:head>
 
 <slot />
