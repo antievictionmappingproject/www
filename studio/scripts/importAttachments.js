0a75d7886812cdbca3bd9345ead36e4e43763ad2
Jonas Luebbers <jonas@jonasluebbers.com>
2022-09-27 16:31:26 -0700

    Run prettier for studio scripts

--- studio/scripts/importAttachments.js
+++ studio/scripts/importAttachments.js
@@ -1,51 +1,66 @@
-import fs from "fs";
-import fsp from "fs/promises";
-import got from "got";
-import { getClient } from "../lib/sanity.js";
-import { eachItem } from "../lib/sqsp.js";
+import fs from 'fs'
+import fsp from 'fs/promises'
+import got from 'got'
+import {getClient} from '../lib/sanity.js'
+import {eachItem} from '../lib/sqsp.js'
 
-const exportFile = new URL("../tmp/export.xml", import.meta.url);
-const attachmentsFile = new URL("../tmp/attachments.json", import.meta.url);
+const exportFile = new URL(
+  '../tmp/export.xml',
+  import.meta.url
+)
+const attachmentsFile = new URL(
+  '../tmp/attachments.json',
+  import.meta.url
+)
 
-const { client, queue } = getClient();
+const {client, queue} = getClient()
 
 async function run() {
-  const ids = new Map();
+  const ids = new Map()
 
   await eachItem(fs.createReadStream(exportFile), (item) => {
     const {
-      "wp:attachment_url": url,
-      "wp:post_type": type,
-      "wp:post_id": id,
-    } = item;
-    if (type === "attachment" && url) {
+      'wp:attachment_url': url,
+      'wp:post_type': type,
+      'wp:post_id': id
+    } = item
+    if (type === 'attachment' && url) {
       queue.add(async () => {
-        console.log(`Attachment ${id}: Started`);
+        console.log(`Attachment ${id}: Started`)
         try {
           const readStream = got
-            .stream(url, { timeout: { request: 10000 } })
-            .on("error", () => {
-              readStream.destroy(`Attachment ${id}: Failed to get`);
-            });
-          const document = await client.assets.upload("image", readStream, {
-            filename: item.title,
-          });
-          ids.set(id, document._id);
+            .stream(url, {timeout: {request: 10000}})
+            .on('error', () => {
+              readStream.destroy(
+                `Attachment ${id}: Failed to get`
+              )
+            })
+          const document = await client.assets.upload(
+            'image',
+            readStream,
+            {
+              filename: item.title
+            }
+          )
+          ids.set(id, document._id)
         } catch (error) {
-          console.log(`Attachment ${id}: ${error.message}`);
+          console.log(`Attachment ${id}: ${error.message}`)
         } finally {
-          console.log(`Attachment ${id}: Completed`);
+          console.log(`Attachment ${id}: Completed`)
         }
-      });
+      })
     }
-  });
+  })
 
-  queue.on("idle", () => {
-    console.log("Idle");
-    fsp.writeFile(attachmentsFile, JSON.stringify(Array.from(ids)));
-  });
+  queue.on('idle', () => {
+    console.log('Idle')
+    fsp.writeFile(
+      attachmentsFile,
+      JSON.stringify(Array.from(ids))
+    )
+  })
 
-  queue.start();
+  queue.start()
 }
 
-run();
+run()
--- studio/scripts/importPosts.js
+++ studio/scripts/importPosts.js
@@ -1,108 +1,124 @@
-import fsp from "fs/promises";
-import fs from "fs";
-import slugifyHelper from "slugify";
-import blockTools from "@sanity/block-tools";
-import { JSDOM } from "jsdom";
-import { eachItem } from "../lib/sqsp.js";
-import { getSchema } from "../lib/sanity.js";
+import fsp from 'fs/promises'
+import fs from 'fs'
+import slugifyHelper from 'slugify'
+import blockTools from '@sanity/block-tools'
+import {JSDOM} from 'jsdom'
+import {eachItem} from '../lib/sqsp.js'
+import {getSchema} from '../lib/sanity.js'
 
-const exportFile = new URL("../tmp/export.xml", import.meta.url);
-const attachmentsFile = new URL("../tmp/attachments.json", import.meta.url);
+const exportFile = new URL(
+  '../tmp/export.xml',
+  import.meta.url
+)
+const attachmentsFile = new URL(
+  '../tmp/attachments.json',
+  import.meta.url
+)
 
-const schema = getSchema();
+const schema = getSchema()
 
 function slugify(string) {
-  return slugifyHelper(string, { strict: true });
+  return slugifyHelper(string, {strict: true})
 }
 
 function writeLine(object) {
-  process.stdout.write(JSON.stringify(object) + "\n");
+  process.stdout.write(JSON.stringify(object) + '\n')
 }
 
 export async function run() {
   const authors = new Set(),
     locations = new Set(),
-    tags = new Set();
-  const attachments = new Map(JSON.parse(await fsp.readFile(attachmentsFile)));
+    tags = new Set()
+  const attachments = new Map(
+    JSON.parse(await fsp.readFile(attachmentsFile))
+  )
   await eachItem(fs.createReadStream(exportFile), (item) => {
-    if (item["wp:post_type"] === "post") {
-      authors.add(item["dc:creator"]);
+    if (item['wp:post_type'] === 'post') {
+      authors.add(item['dc:creator'])
       for (let location of item.category ?? []) {
-        locations.add(location);
+        locations.add(location)
       }
       for (let tag of item.post_tag ?? []) {
-        tags.add(tag);
+        tags.add(tag)
       }
       writeLine({
-        _type: "post",
+        _type: 'post',
         title: {
-          en: item.title,
+          en: item.title
         },
         slug: {
-          current: slugify(item.title),
+          current: slugify(item.title)
         },
         author: {
-          _type: "reference",
-          _ref: slugify("author-" + item["dc:creator"]),
+          _type: 'reference',
+          _ref: slugify('author-' + item['dc:creator'])
         },
         mainImage: attachments.has(item._thumbnail_id)
           ? {
-              _type: "image",
+              _type: 'image',
               asset: {
                 _ref: attachments.get(item._thumbnail_id),
-                _type: "reference",
-              },
+                _type: 'reference'
+              }
             }
           : undefined,
         locations: item.category?.map((location) => ({
-          _type: "reference",
-          _ref: slugify("location-" + location),
+          _type: 'reference',
+          _ref: slugify('location-' + location)
         })),
         tags: item.post_tag?.map((tag) => ({
-          _type: "reference",
-          _ref: slugify("tag-" + tag),
+          _type: 'reference',
+          _ref: slugify('tag-' + tag)
         })),
-        datePublished: new Date(item["wp:post_date"]).toISOString(),
-        dateUpdated: new Date(item["wp:post_date"]).toISOString(),
+        datePublished: new Date(
+          item['wp:post_date']
+        ).toISOString(),
+        dateUpdated: new Date(
+          item['wp:post_date']
+        ).toISOString(),
         body: {
           en: blockTools.htmlToBlocks(
-            item["content:encoded"],
+            item['content:encoded'],
             schema
-              .get("post")
-              .fields.find((field) => field.name === "body")
-              .type.fields.find((field) => field.name === "en").type,
-            { parseHtml: (html) => new JSDOM(html).window.document }
-          ),
-        },
-      });
+              .get('post')
+              .fields.find((field) => field.name === 'body')
+              .type.fields.find((field) => field.name === 'en')
+              .type,
+            {
+              parseHtml: (html) =>
+                new JSDOM(html).window.document
+            }
+          )
+        }
+      })
     }
-  });
+  })
 
   for (let author of authors) {
     writeLine({
-      _type: "author",
-      _id: slugify("author-" + author),
-      name: author,
-    });
+      _type: 'author',
+      _id: slugify('author-' + author),
+      name: author
+    })
   }
 
   for (let location of locations) {
     writeLine({
-      _type: "location",
-      _id: slugify("location-" + location),
-      title: location,
-    });
+      _type: 'location',
+      _id: slugify('location-' + location),
+      title: location
+    })
   }
 
   for (let tag of tags) {
     writeLine({
-      _type: "tag",
-      _id: slugify("tag-" + tag),
+      _type: 'tag',
+      _id: slugify('tag-' + tag),
       title: {
-        en: tag,
-      },
-    });
+        en: tag
+      }
+    })
   }
 }
 
-run();
+run()
