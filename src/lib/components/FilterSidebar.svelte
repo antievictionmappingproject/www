3601eb0addb23b2fa81a641afe9eab78f9bf2717
Jonas Luebbers <jonas@jonasluebbers.com>
2022-10-11 15:46:19 -0700

    Add clear search button

--- src/lib/components/FilterSidebar.svelte
+++ src/lib/components/FilterSidebar.svelte
@@ -1,18 +1,27 @@
 <script lang="ts">
+  import {page} from '$app/stores'
   import {locale} from '$i18n/i18n-svelte'
   import uiClasses from '$lib/ui.module.css'
+  import {withoutSearchParam} from '$lib/url'
 
   export let query: string | undefined = undefined
 </script>
 
 <form action={`/${$locale}/search`} method="get">
-  {#if query}
-    <input type="text" name="query" value={query} hidden />
-  {/if}
   <slot />
-  <button type="submit" class={uiClasses.input}>
-    Apply Filters
-  </button>
+  <div class="buttons">
+    {#if query}
+      <input type="text" name="query" value={query} hidden />
+      <a
+        class={uiClasses.input}
+        href={withoutSearchParam($page.url, 'query').href}
+        >Clear search</a
+      >
+    {/if}
+    <button type="submit" class={uiClasses.input}>
+      Apply filters
+    </button>
+  </div>
 </form>
 
 <style>
@@ -21,5 +30,11 @@
     flex-direction: column;
     gap: var(--spacing-1);
     min-width: 10rem;
+  }
+
+  .buttons {
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing-minus-1);
   }
 </style>
